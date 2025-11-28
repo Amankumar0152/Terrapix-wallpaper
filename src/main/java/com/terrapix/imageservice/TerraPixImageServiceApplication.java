ee87d7fef9b1070b4fd75bce906d79270a7665a4
Aman Kumar <amankumarmaurya93@gmail.com>
2025-11-28 20:46:27 +0530

    Terrapix wallpaper app

---
+++ src/main/java/com/terrapix/imageservice/TerraPixImageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.terrapix.imageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraPixImageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraPixImageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terrapix/imageservice/TerraPixImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terrapix.imageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraPixImageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
